54e5fe8ac03f3a92ac19699a52d02ae88c3a6ed5
Daniel Ortiz <danielortiz08@gmail.com>
2019-11-17 14:54:41 -0500

    Add files

---
+++ palindromeApp/app.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('.enter')
+const result = document.querySelector('.result')
+const reset = document.querySelector('.reset')
+
+function palindrome(pal) {
+  let noSpaces = pal.replace(/\s/g, '')
+  let lowerCaseReverse = noSpaces.toLowerCase().split('').reverse().join('')
+  if(pal == 0){
+    alert('Please type in the input value to check if it is a palindrome')
+  }else if (pal === lowerCaseReverse){
+    result.innerHTML = 'THIS IS A PALINDROME';
+  }else{
+    result.innerHTML = 'THIS IS NOT A PALINDROME';
+  }
+}
+
+btn.addEventListener('click', () => {
+  let input = document.querySelector('.input').value
+  palindrome(input);
+})
